2cf7e8bc366a20c036699a6ce936cf207564fb43
Sean Midford <seanmidford@outlook.com>
2019-11-03 12:49:25 -0500

    Wrote diff logic unit tests.

--- test/youtube.test.py
+++ test/youtube.test.py
@@ -9,16 +9,66 @@ from youtube import find_renamed_items
 class TestYoutubeDiff(unittest.TestCase):
 
     def setUp(self):
-        print("setup")
+        self.old_items = [
+            ["", "00000000000", "Item 0"],
+            ["", "00000000001", "Item 1"],
+            ["", "00000000002", "Item 2"],
+            ["", "00000000003", "Item 3"],
+            ["!", "00000000004", "Item 4"]
+        ]
+        self.new_items = {
+            "00000000000": "Item 0 (New)",
+            "00000000001": "Item 1",
+            "00000000002": "Item 2",
+            "00000000005": "Item 5"
+        }
 
     def test_added_items(self):
-        print("add test")
+        expected = [
+            ("00000000005", "Item 5")
+        ]
+        actual = find_added_items(self.old_items, self.new_items)
+
+        self.assertEqual(expected, actual)
+
+    def test_added_items_empty(self):
+        expected = [
+            ("00000000000", "Item 0 (New)"),
+            ("00000000001", "Item 1"),
+            ("00000000002", "Item 2"),
+            ("00000000005", "Item 5")
+        ]
+        actual = find_added_items([], self.new_items)
+
+        self.assertEqual(expected, actual)
 
     def test_missing_items(self):
-        print("missing test")
+        expected = [
+            ("00000000003", "Item 3")
+        ]
+        actual = find_missing_items(self.old_items, self.new_items)
+
+        self.assertEqual(expected, actual)
+
+    def test_missing_items_empty(self):
+        expected = []
+        actual = find_missing_items([], self.new_items)
+
+        self.assertEqual(expected, actual)
 
     def test_renamed_items(self):
-        print("renamed test")
+        expected = [
+            ("00000000000", "Item 0", "Item 0 (New)"),
+        ]
+        actual = find_renamed_items(self.old_items, self.new_items)
+
+        self.assertEqual(expected, actual)
+
+    def test_renamed_items_empty(self):
+        expected = []
+        actual = find_renamed_items([], self.new_items)
+
+        self.assertEqual(expected, actual)
 
 if __name__=='__main__':
     unittest.main()
